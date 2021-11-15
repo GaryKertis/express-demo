adfe9bfab59bc2bdf506b51fe13b894774df6fbd
Gary Kertis <gary.kertis@gmail.com>
2021-11-15 15:08:46 -0500

    Finished demo

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 let express = require("express");
 
 const app = express();
+const morgan = require("morgan");
+app.use(express.static("public"));
+app.use(morgan("dev"));
 
-app.get('/', (req, res) => res.send("ok"));
+app.get("/test", (req, res) =>
+  res.send(" <h1 style='color: green;'>Hello</h1> ")
+);
 
-
-app.listen(3000);
+app.get("/favorite-movie/:name/otherthing/:age", (req, res) => {
+  console.log(req.params);
+  res.status(200);
+  if (req.params.name === "Gary") {
+    res.send("The Big Lebowski");
+    return;
+  } else if (req.params.name === "Pete") {
+    res.send("The Penguins of Madagascar");
+    return;
+  }
+  if (req.params.age < 13) {
+    res.send("Must be a PG movie please");
+    return;
+  }
+});
+app.listen(3000, () => {
+  console.log("Running on port 3000");
+});
